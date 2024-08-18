a933a1a2869008ad50ccc2406c4d5f2da36689ad
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2024-08-18 22:26:11 +0300

    feat: finalize configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,17 @@
+# Ctrl + F - autocomplete suggestion variant
+# Ctrl + L - clear
+
+# Ctrl + U - clear all from current position to start
+# Ctrl + K - clear all from current position to end
+# Ctrl + W - clear previous word
+
+# Ctrl + A - move to start
+# Ctrl + E - move to end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    alias H="Hyprland"
-    alias DD="dragon-drop -x -T"
-    set -g fish_greeting ""
 end
+
+alias H="Hyprland"
+alias DD="dragon-drop -x -T"
+set -g fish_greeting ""
